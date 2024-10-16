74430c36794f8addaadb8638b7695df6302c160b
Giorgio-Developer <gaswebmaster@gmail.com>
2024-10-16 11:47:34 +0200

    setFieldValue( ) update 1

--- wordpress/booking-api_to_cf7.js
+++ wordpress/booking-api_to_cf7.js
@@ -14,6 +14,25 @@ document.addEventListener('DOMContentLoaded', function() {
         let parts = dateString.split('-');
         return parts[2] + '-' + parts[1] + '-' + parts[0];
     }
+	
+	function setFieldValue(parameterName) {
+		
+		// parameterName = adults
+		let value = getUrlParameter(parameterName);
+		let inputField = document.getElementById(parameterName);
+		let text = document.getElementById(parameterName+'Text');
+		
+		if(value) {
+			if(inputField)
+				inputField.value = value;
+			if(text) {
+				if(parameterName == 'checkin' || parameterName == 'checkout')
+					text.innerHTML = convertDateFormat(value);
+				else
+					text.innerHTML = value;
+			}
+		}
+	}
 
     const roomsImages = {
         "Villa Panorama": "Villa_Panorama_Suite.jpg",
@@ -26,46 +45,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Ottieni i valori dai parametri dell'URL
     let room = getUrlParameter('room');
-    let checkin = getUrlParameter('checkin');
-    let checkout = getUrlParameter('checkout');
-    let adults = getUrlParameter('adults');
-    let children = getUrlParameter('children');
-    let pets = getUrlParameter('pets');
-    let price = getUrlParameter('price');
-
     let roomImage = document.getElementById('roomImage');
-	let checkinInput = document.getElementById('checkin');
-	let checkinText = document.getElementById('checkinText');
-	let checkoutInput = document.getElementById('checkout');
-	let checkoutText = document.getElementById('checkoutText');
-	let adultsInput = document.getElementById('adults');
-	let adultsText = document.getElementById('adultsText');
-	let roomInput = document.getElementById('room');
-	let roomText = document.getElementById('roomText');
 	let roomName = document.getElementById('roomName');
-	let childrenInput = document.getElementById('children');
-	let childrenText = document.getElementById('childrenText');
-	let petsInput = document.getElementById('pets');
-	let petsText = document.getElementById('petsText');
-	
-	function setFieldValue(parameterName) {
-		
-		// parameterName = adults
-		let value = getUrlParameter(parameterName);
-		let inputField = document.getElementById(parameterName);
-		let text = document.getElementById(parameterName+'Text');
-		
-		if(value) {
-			if(inputField)
-				inputField.value = value;
-			if(text)
-				if(parameterName == 'checkin' || parameterName == 'checkout')
-					text.innerHTML = convertDateFormat(value);
-				else
-					text.innerHTML = value;
-		}
-	}
-	
+	let price = getUrlParameter('price');
 	
     if (roomImage) {
 		if (room) 
@@ -75,41 +57,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
     // Precompila i campi del form e rendili non modificabili
-    if (checkin) {
-        checkinInput.value = checkin;       
-        checkinText.innerHTML = convertDateFormat(checkin);
-    }
-
-    if (checkout) {    
-        checkoutInput.value = checkout;    
-        checkoutText.innerHTML = convertDateFormat(checkout);
-    }
-/*
-    if(adults) {
-		if (adultsInput)
-        	adultsInput.value = adults;
-        adultsText.innerHTML = adults;
-    }
-*/
-	
+	setFieldValue('checkin');
+	setFieldValue('checkout');
 	setFieldValue('adults');
-	
-    if(room ) {
-        roomInput.value = room;
-        roomText.innerHTML = room;
-        roomName.innerHTML = room;
-    }
-
-    if(children) {
-        childrenInput.value = children;
-        childrenText.innerHTML = children;
-    }
-
-    if(pets) {
- 
-        petsInput.value = pets;
-        petsText.innerHTML = pets;
-    }
+	setFieldValue('room');
+	setFieldValue('children');
+	setFieldValue('pets');
 
     if(price) {
         let priceInput = document.getElementById('price');
